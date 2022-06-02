52d4de0a0dcbc7c52a39aaf9cc241c2504de1211
OkelloJP <okello.j30@yahoo.com>
2022-06-02 14:43:28 -0700

    UNIX is a simple Os

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -17,12 +17,11 @@ int main(void)
 
 
 
-			write(2,
+		write(2,
 
-						      "and that piece of art is useful - Dora Korpar, 2015-10-19\n",
+		        "and that piece of art is useful - Dora Korpar, 2015-10-19\n",
+	      	      s);
 
-						      	      s);
-
-				return (1);
+		return (1);
 
 }
